3b92f90ebe5b27b1c34185276bf63956fbf3a0d4
Nithish K S <81469801+ksnithish27@users.noreply.github.com>
2023-02-24 17:57:03 +0530

    Add files via upload

--- Program208A.java
+++ Program208A.java
@@ -4,9 +4,33 @@ public class Program208A
 	public static int diff(int a, int b){
 		return a-b;
 	}
-
-}
-
+	public static int prod(int a, int b){
+		return a*b;
+	}
+	public static int smaller(int a, int b)
+	{
+		if(a==b){
+			return 0;
+		}
+		else if(a<b){
+			return a;
+		}
+		else {
+			return b;
+		}
+	}
+	public static int larger(int a, int b)
+	{
+		if(a==b){
+			return 0;
+		}
+		else if(a>b){
+			return a;
+		}
+		else {
+			return b;
+		}
+	}
 	public static void main(String[] args) 
 	{
 		Scanner sc = new Scanner (System.in);
@@ -14,7 +38,11 @@ public class Program208A
 		int a = sc.nextInt();
 		System.out.println("Enter the value of b");
 		int b = sc.nextInt();
-		System.out.println(diff(a,b));
-		
+		int difference = diff(a,b);
+		System.out.println("diffrence = "+difference);
+		int product = prod(a,b);
+		System.out.println("Product = "+product);
+		System.out.println("smaller = "+smaller(a,b));
+		System.out.println("larger = "+larger(a,b));
 	}
 }
